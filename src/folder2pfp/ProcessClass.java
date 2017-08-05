e60268d4d87ff95377ec540be7424ece5cc5130f
Ankit <ankit4u3@gmail.com>
2017-08-05 12:52:39 +0530

    Program to Read a Directory of Image and Convert into PDF Developer Ankit Prasad

---
+++ src/folder2pfp/ProcessClass.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package folder2pfp;
+
+/**
+ *
+ * @author developeracer
+ */
+public class ProcessClass {
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+ 
+ 
+    
+ 
+    
+    String filename;
+    String filepath;
+    String extension;
+   
+
+     
+}
